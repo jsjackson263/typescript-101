9b5c96d7c337e0d137a30833d2bca7da69c1bacc
jsjackson <jsjackson@example.com>
2021-05-02 17:13:35 +1000

    typescript basics

--- tutorial/main.js
+++ tutorial/main.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = 'Welcome back!';
 console.log(message);
@@ -11,3 +26,119 @@ var total = 0;
 var name = 'Jack';
 var sentence = "My name is " + name + "\nI am a beginner in Typescript";
 console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+//declaring an array type - 2 syntaxes:
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+//tuple:
+var person1 = ['Chris', 22];
+console.log(person1);
+//enum type
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log('Color is:', c); //not printing
+//any type:
+var randomValue = 10;
+randomValue = true; // doesn't throw error
+randomValue = 'Jack';
+var variable = 10;
+console.log(variable.name);
+//unknown type:
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj;
+}
+if (hasName(myVariable)) {
+    console.log(variable.name);
+}
+//(myVariable as string).toUpperCase(); //type casting
+//type inference (reminder: specifying type is optional)
+var a;
+a = 10;
+a = true;
+var b = 20; //when a variable is intialized at declaration, the type is inferred
+//b = true;  error!
+//Union of types (for the same variable):
+var multiType;
+multiType = 20;
+multiType = true;
+//Functions:
+//the JavaScript way:
+function addjs(num1, num2) {
+    return num1 + num2;
+}
+console.log('JS:', addjs(5, 10));
+//typescript
+function addts(num1, num2) {
+    return num1 + num2;
+}
+console.log('TS:', addts(5, 10));
+console.log('TS:', addts(45, 50));
+//optional parameters:
+function addts2(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+console.log('TS2 Optional:', addts2(5));
+//default parameters:
+function addts3(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+console.log('TS3 Default:', addts3(5, 25));
+console.log('TS3 Default:', addts3(5));
+//Interfaces:
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullName(p);
+function fullName2(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+fullName(p);
+//Classes
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Bruce');
+emp1.greet();
+//inheritence
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this; //call the base class constructor
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Manager Jack');
+m1.delegateWork();
+m1.greet();
--- tutorial/main.ts
+++ tutorial/main.ts
@@ -15,3 +15,156 @@ let name:string = 'Jack';
 let sentence: string = `My name is ${name}
 I am a beginner in Typescript`
 console.log(sentence)
+
+let n: null = null;
+let u : undefined = undefined
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+//declaring an array type - 2 syntaxes:
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+//tuple:
+let person1: [string, number] = ['Chris', 22];
+console.log(person1);
+
+//enum type
+enum Color {Red=5, Green, Blue};
+
+let c:Color = Color.Green;
+console.log('Color is:', c); //not printing
+
+//any type:
+let randomValue: any = 10;
+randomValue = true; // doesn't throw error
+randomValue = 'Jack';
+
+let variable: any = 10; 
+console.log(variable.name);
+
+//unknown type:
+let myVariable: unknown = 10;
+
+function hasName(obj: any): obj is {name: string} {
+    return !!obj &&
+    typeof obj === "object" &&
+    "name" in obj
+}
+
+if (hasName(myVariable)) {
+    console.log(variable.name);
+
+}
+//(myVariable as string).toUpperCase(); //type casting
+
+
+//type inference (reminder: specifying type is optional)
+let a;
+a = 10;
+a = true;
+
+let b = 20; //when a variable is intialized at declaration, the type is inferred
+//b = true;  error!
+
+
+//Union of types (for the same variable):
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+
+//Functions:
+
+//the JavaScript way:
+function addjs(num1, num2) {
+    return num1 + num2;
+}
+console.log('JS:', addjs(5, 10))
+
+//typescript
+function addts(num1: number, num2: number): number {
+    return num1 + num2;
+}
+console.log('TS:', addts(5, 10))
+console.log('TS:', addts(45, 50))
+
+//optional parameters:
+function addts2(num1: number, num2?: number): number {
+    if (num2)
+        return num1 + num2;
+   else
+        return num1;
+}
+console.log('TS2 Optional:', addts2(5))
+
+
+//default parameters:
+function addts3(num1: number, num2: number = 10): number {
+    if (num2)
+        return num1 + num2;
+   else
+        return num1;
+}
+console.log('TS3 Default:', addts3(5, 25))
+console.log('TS3 Default:', addts3(5))
+
+
+//Interfaces:
+function fullName(person: {firstName: string, lastName: string}) {  //what if we had many parameters?
+    console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullName(p);
+
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function fullName2(person: Person) {  
+    console.log(`${person.firstName} ${person.lastName}`)
+}
+fullName(p);
+
+
+//Classes
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`)
+    }
+}
+
+let emp1 = new Employee('Bruce');
+emp1.greet();
+
+
+//inheritence
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName)  //call the base class constructor
+    }
+
+    delegateWork() {
+        console.log(`Manager delegating tasks`);
+
+    }
+}
+
+let m1 = new Manager('Manager Jack');
+m1.delegateWork();
+m1.greet();
+
+
